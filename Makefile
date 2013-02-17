3c9c490ab219fcd24647b4da78ce44de7122cc18
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2013-02-16 22:25:55 -0500

    add random forest training

--- Makefile
+++ Makefile
@@ -1,14 +1,20 @@
-OPENCV=/Users/thouis/homebrew//Cellar/opencv/2.4.2
+OPENCV=/Users/thouis/opencv-install
 HDF5_DIR=/Users/thouis/homebrew/Cellar/hdf5/1
 CXXFLAGS=-I$(OPENCV)/include -I$(HDF5_DIR)/include -g
 LDFLAGS=-L$(OPENCV)/lib -lopencv_highgui -lopencv_imgproc -lopencv_core -L$(HDF5_DIR)/lib -lhdf5_cpp -lhdf5
 
-all: compute_features test_image_to_hdf5
+all: compute_features show_features test_image_to_hdf5 train_randomforest
 
 membrane.o: quickmedian.h
 
 compute_features: opencv_hdf5.o compute_features.o membrane.o adapthisteq.o local_statistics.o tensor_gradient_features.o drawhist.o 
 	g++ -o $@ $^ $(LDFLAGS)
+
+show_features: opencv_hdf5.o show_features.o
+	g++ -o $@ $^ $(LDFLAGS)
+
+train_randomforest: opencv_hdf5.o train_randomforest.o
+	g++ -o $@ $^ $(LDFLAGS) -lopencv_ml
 
 test_image_to_hdf5: opencv_hdf5.o test_image_to_hdf5.o 
 	g++ -o $@ $^ $(LDFLAGS)
--- opencv_hdf5.cpp
+++ opencv_hdf5.cpp
@@ -12,6 +12,11 @@ H5File create_feature_file(char *filename, const Mat &base_image)
     imsize = base_image.size();
     return H5File(filename, H5F_ACC_TRUNC);
 }
+
+H5File open_feature_file(char *filename)
+{
+    return H5File(filename, H5F_ACC_RDONLY);
+}
     
 
 static DataSet create_dataset(H5File h5f, const char *name)
@@ -47,7 +52,6 @@ void write_feature(H5File h5f, const Mat &image_in, const char *name)
     DataSpace imspace;
     float *imdata;
     if (image.isContinuous()) {
-        hsize_t count[2];
         imspace = dataset.getSpace(); // same size as 
         imspace.selectAll();
         imdata = image.ptr<float>();
@@ -66,4 +70,45 @@ void write_feature(H5File h5f, const Mat &image_in, const char *name)
         imdata = image.ptr<float>() - parent_ofs.x - parent_ofs.y * parent_size.width;
     }
     dataset.write(imdata, PredType::NATIVE_FLOAT, imspace);
+}
+
+void read_feature(H5File h5f, Mat &image_out, const char *name)
+{
+    DataSet dataset = h5f.openDataSet(name);
+    DataSpace dspace = dataset.getSpace();
+    assert (dspace.getSimpleExtentNdims() == 2);
+    hsize_t dims[2];
+    dspace.getSimpleExtentDims(dims);
+    image_out.create(dims[0], dims[1], CV_32F);
+    
+    DataSpace imspace;
+    float *imdata;
+    if (image_out.isContinuous()) {
+        imspace = dataset.getSpace(); // same size as 
+        imspace.selectAll();
+        imdata = image_out.ptr<float>();
+    } else {
+        // we are working with an ROI
+        assert (image_out.isSubmatrix());
+        Size parent_size; Point parent_ofs;
+        image_out.locateROI(parent_size, parent_ofs);
+        hsize_t parent_count[2];
+        parent_count[0] = parent_size.height; parent_count[1] = parent_size.width;
+        imspace.setExtentSimple(2, parent_count);
+        hsize_t im_offset[2], im_size[2];
+        im_offset[0] = parent_ofs.y; im_offset[1] = parent_ofs.x;
+        im_size[0] = image_out.size().height; im_size[1] = image_out.size().width;
+        imspace.selectHyperslab(H5S_SELECT_SET, im_size, im_offset);
+        imdata = image_out.ptr<float>() - parent_ofs.x - parent_ofs.y * parent_size.width;
+    }
+    dataset.read(imdata, PredType::NATIVE_FLOAT, imspace);
+}
+
+vector<string> get_feature_names(H5File h5f)
+{
+    vector<string> out;
+    int num_features;
+    for (int i = 0; i < h5f.getNumObjs(); i++)
+        out.push_back(h5f.getObjnameByIdx(i));
+    return out;
 }
